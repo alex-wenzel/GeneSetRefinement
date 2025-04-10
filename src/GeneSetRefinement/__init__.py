85653613cda9d0bb6e50a13e953e2a3cd9e48c29
atwenzel <atwenzel@eng.ucsd.edu>
2025-04-09 18:30:47 -0700

    put version number in one place (__init__)

--- src/GeneSetRefinement/__init__.py
+++ src/GeneSetRefinement/__init__.py
@@ -1,9 +1,11 @@
+__version__ = "1.0.4"
+
 from .Data2D import Data2D, Data2DView
 from .Expression import Expression
 from .GeneSet import GeneSet, read_gmt
 from .Phenotypes import Phenotypes
 from .Refinement import (
-	Refinement, InnerIteration, CombinedGeneComponentIC
+	Refinement, InnerIteration, CombinedGeneComponentIC, 
+	PhenotypeComponentIC
 )
 from .Utils import load_gct, compute_information_coefficient
-from .version import VERSION
